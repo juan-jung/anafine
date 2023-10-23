5683f686495fce9e39c8c6f16187df10199db744
박동휘 <daniel1502@naver.com>
2023-10-23 17:07:48 +0900

    Feat : Organism SearchBar 생성

---
+++ frontend/src/components/Organisms/SearchBar/SearchBar.tsx
@@ -0,0 +1,48 @@
+import React, { ChangeEvent, FormEvent } from "react";
+
+interface SearchBarProps {
+  value: string;
+  onChange: (value: string) => void;
+  onSubmit: (value: string) => void;
+  placeholder: string;
+  disabled?: boolean;
+  id?: string;
+  name?: string;
+}
+
+// SearchBar
+const SearchBar: React.FC<SearchBarProps> = ({
+  value,
+  onChange,
+  onSubmit,
+  placeholder,
+  disabled = false,
+  id,
+  name,
+}) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit(value);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        id={id}
+        name={name}
+      />
+      <button type="submit">검색</button>
+    </form>
+  );
+};
+
+export default SearchBar;

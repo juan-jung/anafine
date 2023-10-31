2f582a4f3d42453c7e6c70446915411aaadc1b50
박동휘 <daniel1502@naver.com>
2023-10-31 12:57:35 +0900

    Feat : api 기능 추가

---
+++ frontend/src/pages/api/map/detail/[id].tsx
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import instance from "pages/axios";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { id },
+  } = req;
+
+  try {
+    const response = await instance.get(`/map/detail/${id}`);
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Error making API request:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+};

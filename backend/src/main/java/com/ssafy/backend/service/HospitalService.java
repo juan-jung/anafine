130da8b2316b3f4056554b80e3eaaded246f36df
이원희 <thehee0407@naver.com>
2023-11-01 10:33:23 +0900

    fix : 병원데이터 전송 시 파라미터 변경

--- backend/src/main/java/com/ssafy/backend/service/HospitalService.java
+++ backend/src/main/java/com/ssafy/backend/service/HospitalService.java
@@ -43,11 +43,11 @@ public class HospitalService {
 	}
 
 	// distance로 정렬된 병원 정보를 return하는 함수 => 거리 제한을 통해 제한된 거리 내에서만 보내줌
-	public List<HospitalInfoDto> showByPrice(String treatmentId, Double disLimit, Double userLatitude,
+	public List<HospitalInfoDto> showByPrice(String name, Double disLimit, Double userLatitude,
 		Double userLongitude) {
 		// 검색한 비급여를 가지고 있는 병원들 추출
-		List<Price> prices = hospitalTreatmentRepository.findByTreatment_IdOrderByMinPriceAsc(
-			treatmentId);
+		List<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
+			name);
 
 		// List<Price;> prices = null;
 		List<HospitalInfoDto> hospitalInfoDtos = new ArrayList<>();
@@ -78,11 +78,11 @@ public class HospitalService {
 
 	}
 
-	public List<HospitalInfoDto> showByDistance(String treatmentId, Double disLimit, Double userLatitude,
+	public List<HospitalInfoDto> showByDistance(String name, Double disLimit, Double userLatitude,
 		Double userLongitude) {
 		// 검색한 비급여를 가지고 있는 병원들 추출
-		List<Price> prices = hospitalTreatmentRepository.findByTreatment_IdOrderByMinPriceAsc(
-			treatmentId);
+		List<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
+			name);
 
 		// List<Price;> prices = null;
 		List<HospitalInfoDto> hospitalInfoDtos = new ArrayList<>();
@@ -113,6 +113,7 @@ public class HospitalService {
 		return hospitalInfoDtos;
 
 	}
+
 
 	private static int locationDistance(double lat1, double lon1, double lat2, double lon2) {
 		double theta = lon1 - lon2;

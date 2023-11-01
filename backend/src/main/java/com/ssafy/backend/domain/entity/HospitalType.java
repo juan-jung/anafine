5ac0e80e287af8216b14a0df599ce8943e4f6de8
이원희 <thehee0407@naver.com>
2023-11-01 17:16:31 +0900

    feat : 페이지네이션 구현

--- backend/src/main/java/com/ssafy/backend/domain/entity/HospitalType.java
+++ backend/src/main/java/com/ssafy/backend/domain/entity/HospitalType.java
@@ -9,6 +9,8 @@ import com.sun.istack.NotNull;
 @Entity
 @Getter
 @Table(name = "hospital_type")
+@AllArgsConstructor
+@NoArgsConstructor
 public class HospitalType {
     @Id
     @Column(name = "hospital_type_id")
--- backend/src/main/java/com/ssafy/backend/service/HospitalService.java
+++ backend/src/main/java/com/ssafy/backend/service/HospitalService.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +15,7 @@ import com.ssafy.backend.dto.HospitalDetailInfoDto;
 import com.ssafy.backend.dto.HospitalInfoDto;
 import com.ssafy.backend.domain.entity.Price;
 import com.ssafy.backend.domain.repository.HospitalTreatmentRepository;
+import com.ssafy.backend.dto.HospitalResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -43,14 +48,16 @@ public class HospitalService {
 	}
 
 	// distance로 정렬된 병원 정보를 return하는 함수 => 거리 제한을 통해 제한된 거리 내에서만 보내줌
-	public List<HospitalInfoDto> showByPrice(String name, Double disLimit, Double userLatitude,
-		Double userLongitude) {
+	public HospitalResponseDto showByPrice(String name, Double disLimit, Double userLatitude,
+		Double userLongitude, int pageNum, int pageSize) {
+		// Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
 		// 검색한 비급여를 가지고 있는 병원들 추출
-		List<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
-			name);
-
+		// Page<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
+		// 	name, pageable);
+		List<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(name);
 		// List<Price;> prices = null;
 		List<HospitalInfoDto> hospitalInfoDtos = new ArrayList<>();
+		int npm= pageNum-1;
 		for (Price price : prices) {
 			// 위도
 			double lat = price.getHospital().getLatitude();
@@ -74,16 +81,43 @@ public class HospitalService {
 				hospitalInfoDtos.add(hospitalInfoDto);
 			}
 		}
-		return hospitalInfoDtos;
+		int te = hospitalInfoDtos.size();
+		HospitalResponseDto hospitalResponseDto = new HospitalResponseDto();
+		if (npm * pageSize <= te && npm!=0) {
+			if ((npm + 1) * pageSize <= te) {
+				hospitalResponseDto.setContent(hospitalInfoDtos.subList(npm * pageSize, (npm + 1) * pageSize));
+				hospitalResponseDto.setTotalElements(te);
+				hospitalResponseDto.setTotalPages((int)Math.ceil((double)te / pageSize));
+			} else {
+				hospitalResponseDto.setContent(hospitalInfoDtos.subList(npm * pageSize, te));
+				hospitalResponseDto.setTotalElements(te);
+				hospitalResponseDto.setTotalPages((int)Math.ceil((double)te / pageSize));
+			}
+		}
+		return hospitalResponseDto;
 
 	}
 
-	public List<HospitalInfoDto> showByDistance(String name, Double disLimit, Double userLatitude,
-		Double userLongitude) {
+	public HospitalResponseDto showByDistance(String name, Double disLimit, Double userLatitude,
+		Double userLongitude, int pageNum, int pageSize) {
 		// 검색한 비급여를 가지고 있는 병원들 추출
-		List<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
-			name);
-
+		// Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+		// Page<HospitalInfoDto> hospitalInfoDtos = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
+		// 	name,pageable).map(price -> HospitalInfoDto.builder().
+		// 	hospitalId(price.getHospital().getId()).
+		// 	hospitalName(price.getHospital().getName()).
+		// 	distance(locationDistance(userLatitude, userLongitude, price.getHospital().getLatitude(), price.getHospital().getLongitude())).
+		// 	longitude(price.getHospital().getLongitude()).
+		// 	latitude(price.getHospital().getLatitude()).
+		// 	priceId(price.getId()).
+		// 	maxPrice(price.getMaxPrice()).
+		// 	minPrice(price.getMinPrice()).
+		// 	treatmentName(price.getTreatment().getName()).
+		// 	build());
+		// Page<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(
+		// 	name, pageable);
+		List<Price> prices = hospitalTreatmentRepository.findByTreatment_NameOrderByMinPriceAsc(name);
+		int npm = pageNum-1;
 		// List<Price;> prices = null;
 		List<HospitalInfoDto> hospitalInfoDtos = new ArrayList<>();
 		for (Price price : prices) {
@@ -106,14 +140,27 @@ public class HospitalService {
 					distance(dis).
 					treatmentName(price.getTreatment().getName()).
 					build();
+
 				hospitalInfoDtos.add(hospitalInfoDto);
 			}
 		}
+		int te = hospitalInfoDtos.size();
 		hospitalInfoDtos.sort(Comparator.comparingDouble(HospitalInfoDto::getDistance));
-		return hospitalInfoDtos;
+		HospitalResponseDto hospitalResponseDto = new HospitalResponseDto();
+		if (npm * pageSize <= te && npm!=0) {
+			if ((npm + 1) * pageSize <= te) {
+				hospitalResponseDto.setContent(hospitalInfoDtos.subList(npm * pageSize, (npm + 1) * pageSize));
+				hospitalResponseDto.setTotalElements(te);
+				hospitalResponseDto.setTotalPages((int)Math.ceil((double)te / pageSize));
+			} else {
+				hospitalResponseDto.setContent(hospitalInfoDtos.subList(npm * pageSize, te));
+				hospitalResponseDto.setTotalElements(te);
+				hospitalResponseDto.setTotalPages((int)Math.ceil((double)te / pageSize));
+			}
+		}
 
+		return hospitalResponseDto;
 	}
-
 
 	private static int locationDistance(double lat1, double lon1, double lat2, double lon2) {
 		double theta = lon1 - lon2;
@@ -124,7 +171,7 @@ public class HospitalService {
 		dist = rad2deg(dist);
 		dist = dist * 60 * 1.1515 * 1609.344;
 
-		return (int) dist; //단위 meter
+		return (int)dist; //단위 meter
 	}
 
 	//10진수를 radian(라디안)으로 변환
@@ -138,3 +185,5 @@ public class HospitalService {
 	}
 
 }
+
+

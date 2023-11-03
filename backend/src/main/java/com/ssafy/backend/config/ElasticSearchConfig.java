cc5de9d1b8054d508a724fce597797e2e4ef3653
juan-jung <crumbled23@gmail.com>
2023-11-03 13:28:38 +0900

    .env 생성

--- backend/src/main/java/com/ssafy/backend/config/ElasticSearchConfig.java
+++ backend/src/main/java/com/ssafy/backend/config/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.backend.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -10,13 +11,21 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.host}")
+    private String host;
+    @Value("${spring.elasticsearch.port}")
+    private String port;
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+    @Value("${spring.elasticsearch.password}")
+    private String password;
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
 
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("k9a403a.p.ssafy.io:9200")
-                .withBasicAuth("elastic","wonhee").build();
+                .connectedTo(host+":"+port)
+                .withBasicAuth(username,password).build();
 
         return RestClients.create(clientConfiguration).rest();
     }

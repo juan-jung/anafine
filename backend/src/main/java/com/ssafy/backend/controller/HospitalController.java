d07f07ae35765548d94d40853cc0e9c261012d92
이원희 <thehee0407@naver.com>
2023-11-06 11:16:00 +0900

    feat : 페이지네이션 구현 및 에러처리

--- backend/src/main/java/com/ssafy/backend/controller/HospitalController.java
+++ backend/src/main/java/com/ssafy/backend/controller/HospitalController.java
@@ -2,7 +2,6 @@ package com.ssafy.backend.controller;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.backend.dto.HospitalDetailInfoDto;
 import com.ssafy.backend.dto.HospitalInfoDto;
-import com.ssafy.backend.dto.HospitalResponseDto;
+import com.ssafy.backend.exception.CustomException;
+import com.ssafy.backend.exception.ExceptionEnum;
 import com.ssafy.backend.service.HospitalService;
 
 import lombok.RequiredArgsConstructor;
@@ -28,30 +28,26 @@ public class HospitalController {
 
 	@GetMapping("/sortByDistInfo")
 	public ResponseEntity<?> getSortByDistHospitalInfo(
-		@RequestParam String name,
+		@RequestParam String treatmentId,
 		@RequestParam Double disLimit,
 		@RequestParam Double userLatitude,
-		@RequestParam Double userLongitude,
-		@RequestParam int pageNum,
-		@RequestParam int pageSize) {
-		HospitalResponseDto hospitalResponseDto = hospitalService.showByDistance(name, disLimit, userLatitude,
+		@RequestParam Double userLongitude, @RequestParam int pageNum, @RequestParam int pageSize) {
+		if(pageNum<0) throw new CustomException(ExceptionEnum.PAGEINDEX_ERROR);
+		List<HospitalInfoDto> hospitalInfoDtos = hospitalService.showByDistance(treatmentId, disLimit, userLatitude,
 			userLongitude, pageNum, pageSize);
-		// return ResponseEntity.ok().body(hospitalService.showByDistance(name, disLimit, userLatitude,
-		// 	userLongitude, pageNum, pageSize).map(price -> price.getId()));
-		return ResponseEntity.ok().body(hospitalResponseDto);
+		return ResponseEntity.ok().body(hospitalInfoDtos);
 	}
 
 	@GetMapping("/sortByPriceInfo")
 	public ResponseEntity<?> getSortByPriceHospitalInfo(
-		@RequestParam String name,
+		@RequestParam String treatmentId,
 		@RequestParam Double disLimit,
 		@RequestParam Double userLatitude,
 		@RequestParam Double userLongitude, @RequestParam int pageNum, @RequestParam int pageSize) {
-		// Page<HospitalInfoDto> hospitalInfoDtos = hospitalService.showByPrice(name, disLimit, userLatitude,
-		// 	userLongitude, pageNum, pageSize);
-		HospitalResponseDto hospitalResponseDto = hospitalService.showByPrice(name, disLimit, userLatitude,
+		if(pageNum<0) throw new CustomException(ExceptionEnum.PAGEINDEX_ERROR);
+		List<HospitalInfoDto> hospitalInfoDtos = hospitalService.showByPrice(treatmentId, disLimit, userLatitude,
 			userLongitude, pageNum, pageSize);
-		return ResponseEntity.ok().body(hospitalResponseDto);
+		return ResponseEntity.ok().body(hospitalInfoDtos);
 	}
 
 	@GetMapping("/detail/{priceId}")

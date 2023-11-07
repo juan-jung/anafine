72a293254c055271f024a38042f3f2223039386d
김민우 <jerry142857@naver.com>
2023-11-07 17:30:04 +0900

    Update Find Path Codes.

---
+++ backend/src/main/java/com/ssafy/backend/dto/PathDto.java
@@ -0,0 +1,45 @@
+package com.ssafy.backend.dto;
+
+import com.ssafy.backend.domain.entity.Category;
+import com.ssafy.backend.domain.entity.Treatment;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PathDto {
+    // CategoryDto
+    private String categoryId;
+
+    private String parentCategory;
+
+    private String name;
+
+    private String info;
+
+    private Boolean isLeaf;
+
+    // TreatmentDto
+    private String treatmentId;
+
+//    private String categoryId;
+
+//    private String name;
+
+//    private String info;
+
+    private String path;
+
+    public static PathDto entityToDto(Category category, Treatment treatment) {
+        PathDto pathDto = PathDto.builder()
+                .categoryId(category.getId())
+                .name(category.getName())
+                .info(category.getInfo())
+                .isLeaf(category.getIsLeaf())
+                .treatmentId(treatment.getId())
+                .path(treatment.getPath())
+                .build();
+
+        return pathDto;
+    }
+}

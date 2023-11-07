fe3dacc84519513fb946f67d4093d539cfd75628
박동휘 <daniel1502@naver.com>
2023-11-07 09:52:23 +0900

    Fix : URL 경로수정

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://www.anafine.com/api",
 });
 
 export default axiosInstance;

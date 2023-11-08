78ded786dbcba7833b7a00b6e00a5547bcda21e5
박종민 <superfoward@naver.com>
2023-11-08 09:04:44 +0900

    fix:class to className

--- frontend/src/pages/info/index.tsx
+++ frontend/src/pages/info/index.tsx
@@ -38,7 +38,7 @@ const InfoPage: NextPage = () => {
       <section className={"infoSection"}>
         <div className={"infoContent"}>
           <img src="/infoPic/man.png" alt="남자" />
-          <div class={"bubble right"}>그럼 제가 내야하는 병원비는 어떻게 되나요?</div>
+          <div className={"bubble right"}>그럼 제가 내야하는 병원비는 어떻게 되나요?</div>
         </div>
         <div className={"infoContent"}>
           <img src="/infoPic/pic6.png" alt="설명6" />
@@ -49,16 +49,15 @@ const InfoPage: NextPage = () => {
       <section className={"infoSection"}>
         <div className={"infoContent"}>
           <img src="/infoPic/man.png" alt="남자" />
-          <div class={"bubble right"}>비급여 항목은 병원마다 가격 차이가 있는데 쉽게 알 수 있는 방법이 없을까요?</div>
+          <div className={"bubble right"}>비급여 항목은 병원마다 가격 차이가 있는데 쉽게 알 수 있는 방법이 없을까요?</div>
         </div>
         <div className={"infoContent"}>
           <img src="/infoPic/advocate.png" alt="여자" />
-          <div class={"bubble left"}>"아나파잉" 서비스를 통해 쉽게 비급여에 관한 병원 및 기타 정보를 얻을 수 있어요!</div>
+          <div className={"bubble left"}>"아나파잉" 서비스를 통해 쉽게 비급여에 관한 병원 및 기타 정보를 얻을 수 있어요!</div>
+          <div className={"bubble left"}>비급여 항목은 병원 자체적으로 진료비를 정하기 때문에 금액 차이가 크게 발생하게 됩니다!</div>
         </div>
         <img src="/infoPic/pic8.png" alt="설명8" />
       </section>  
-
-
       </main>
     </div>
   );

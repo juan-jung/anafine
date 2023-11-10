ba4d6046270b594b4fccaacca2d7a7721ac3e4f4
이원희 <thehee0407@naver.com>
2023-11-10 10:22:26 +0900

    feat : prompt 작성

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -14,13 +14,15 @@ from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from transformers import pipeline
+from langchain.chains.conversation.memory import ConversationBufferMemory
 
 llm = OpenAI(temperature=0)
 
 tools = load_tools(["serpapi", "llm-math"], llm=llm)
 
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-user_input = input("불편한 부위를 자세하게 입력 해주세요 : ex) 왼쪽 아랫배가 아파요.")
 
 import requests
 
@@ -45,46 +47,24 @@ def translate_text(text, source_lang, target_lang, client_id, client_secret):
         print("Error Code:", response.status_code)
         return None
 
-# 사용자 입력을 영어로 번역하기
-# 여기에서 problem 1,2를 번역함
-translated_input = translate_text(user_input, "ko", "en", papago_client_id, papago_client_secret)
-
-
 def get_search_results(query, api_key, language="en"):
     params = {
-        "engine": "google",  # 검색 엔진으로 구글 사용
-        "q": query,          # 검색어
-        "api_key": api_key,  # API 키
-        "hl": language       # 검색 결과 언어 설정
+        "engine": "google",
+        "q": query,
+        "api_key": api_key,
+        "hl": language
     }
     
     try:
         response = requests.get("https://serpapi.com/search", params=params)
-        response.raise_for_status()  # 오류가 있을 경우 예외를 발생시킴
+        response.raise_for_status()
         search_results = response.json()
         return search_results
     except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")  # HTTP 에러 발생시 출력
+        print(f"HTTP error occurred: {http_err}")
     except Exception as err:
-        print(f"An error occurred: {err}")  # 그 외 에러 발생시 출력
-    
-    return None  # 에러 발생시 None 반환
-
-
-
-# 영어로 검색하여 응답 받기
-english_search_results = get_search_results(translated_input, serpapi_api_key, language="en")
-
-# 한국어로 검색하여 응답 받기
-korean_search_results = get_search_results(user_input, serpapi_api_key, language="ko")
-
-english_results_text = ' '.join([result['snippet'] for result in english_search_results.get('organic_results', [])])
-
-# 한국어 검색 결과에서 'organic_results' 부분 추출
-korean_results_text = ' '.join([result['snippet'] for result in korean_search_results.get('organic_results', [])])
-
-# 검색 결과를 통합
-combined_results = english_results_text + " " + korean_results_text
+        print(f"An error occurred: {err}")
+    return None
 
 def preprocess_prompt(prompt):
     # 모델에 입력할 수 있는 토큰의 최대 수
@@ -99,10 +79,53 @@ def preprocess_prompt(prompt):
         # 초과하지 않는다면 그대로 반환
         return prompt
 
-# 문자열 데이터로부터 전처리된 프롬프트를 생성합니다.
-preprocessed_prompt = preprocess_prompt(combined_results)
-# 통합된 검색 결과를 LLM에게 전달하여 분석
-llm_response = agent.run({"input": preprocessed_prompt})
 
-# 최종적으로 분석된 결과 출력
-print(llm_response)
+
+while True:
+    #사용자 성별 입력 받기
+    user_sex = input("당신의 성별을 알려주세요 ex) 여자 : ")
+     # 사용자 나이 입력 받기
+    user_age = input("당신의 나이를 알려주세요 ex) 28 : ")
+    # 아픈 부위 입력 받기
+    user_pain_area = input("아픈 부위를 알려주세요 ex) 왼쪽 아랫 배 : ")
+    # 증상 입력 받기
+    user_symptoms = input("증상을 입력해주세요 ex) 아픔, 콕콕 쑤심, 찌릿함 : ")
+
+    # 원인과, 의심되는 병으로 나누어서 구글 검색
+    user_input_cause = f"{user_age}, {user_sex},  {user_pain_area},  {user_symptoms}, 원인"
+    user_input_disease = f"{user_age}, {user_sex},  {user_pain_area},  {user_symptoms}, 의심 병"
+
+    translated_input_cause = translate_text(user_input_cause, "ko", "en", papago_client_id, papago_client_secret)
+    translated_input_disease = translate_text(user_input_disease, "ko", "en", papago_client_id, papago_client_secret)
+
+    # 영어로 검색하여 응답 받기
+    english_search_results_cause = get_search_results(translated_input_cause, serpapi_api_key, language="en")
+    english_search_results_disease = get_search_results(translated_input_disease, serpapi_api_key, language="en")
+
+    # 한국어로 검색하여 응답 받기
+    korean_search_results_cause = get_search_results(user_input_cause, serpapi_api_key, language="ko")
+    korean_search_results_disease = get_search_results(user_input_disease, serpapi_api_key, language="ko")
+
+    # 영어 검색 결과에서 organic_results 부분 추출
+    english_results_text_cause = ' '.join([result['snippet'] for result in english_search_results_cause.get('organic_results', [])])
+    english_results_text_disease = ' '.join([result['snippet'] for result in english_search_results_disease.get('organic_results', [])])
+
+    # 한국어 검색 결과에서 'organic_results' 부분 추출
+    korean_results_text_cause = ' '.join([result['snippet'] for result in korean_search_results_cause.get('organic_results', [])])
+    korean_results_text_disease = ' '.join([result['snippet'] for result in korean_search_results_disease.get('organic_results', [])])
+    # 검색 결과를 통합
+    combined_results = english_results_text_cause+" "+ english_results_text_disease + " " + korean_results_text_cause + " " + korean_results_text_disease
+
+    # 문자열 데이터로부터 전처리된 프롬프트를 생성
+    preprocessed_prompt = preprocess_prompt(combined_results)
+    # 통합된 검색 결과를 LLM에게 전달하여 분석
+    llm_response = agent.run({"input": preprocessed_prompt})
+
+    # 최종적으로 분석된 결과 출력
+    print(llm_response)
+    keep_going = input("계속하시겠습니까? : ")
+    if(keep_going == "아니오"):
+        break
+
+
+

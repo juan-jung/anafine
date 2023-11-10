a2d5000942cd86a422feab7450f9995448593c7b
이원희 <thehee0407@naver.com>
2023-11-10 12:43:58 +0900

    feat : prompt 추가

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -79,8 +79,6 @@ def preprocess_prompt(prompt):
         # 초과하지 않는다면 그대로 반환
         return prompt
 
-
-
 while True:
     #사용자 성별 입력 받기
     user_sex = input("당신의 성별을 알려주세요 ex) 여자 : ")
@@ -92,37 +90,54 @@ while True:
     user_symptoms = input("증상을 입력해주세요 ex) 아픔, 콕콕 쑤심, 찌릿함 : ")
 
     # 원인과, 의심되는 병으로 나누어서 구글 검색
-    user_input_cause = f"{user_age}, {user_sex},  {user_pain_area},  {user_symptoms}, 원인"
-    user_input_disease = f"{user_age}, {user_sex},  {user_pain_area},  {user_symptoms}, 의심 병"
+    user_input_cause = f"{user_age}세, {user_sex},  {user_pain_area},  {user_symptoms}, 원인"
+    user_input_disease = f"{user_age}세, {user_sex},  {user_pain_area},  {user_symptoms}, 의심 병"
+    user_input_doubt = f"{user_age}세, {user_sex},  {user_pain_area},  {user_symptoms}, 검사"
 
     translated_input_cause = translate_text(user_input_cause, "ko", "en", papago_client_id, papago_client_secret)
     translated_input_disease = translate_text(user_input_disease, "ko", "en", papago_client_id, papago_client_secret)
+    translated_input_doubt = translate_text(user_input_doubt, "ko", "en", papago_client_id, papago_client_secret)
 
     # 영어로 검색하여 응답 받기
     english_search_results_cause = get_search_results(translated_input_cause, serpapi_api_key, language="en")
     english_search_results_disease = get_search_results(translated_input_disease, serpapi_api_key, language="en")
+    english_search_results_doubt = get_search_results(translated_input_doubt, serpapi_api_key, language="en")
 
     # 한국어로 검색하여 응답 받기
     korean_search_results_cause = get_search_results(user_input_cause, serpapi_api_key, language="ko")
     korean_search_results_disease = get_search_results(user_input_disease, serpapi_api_key, language="ko")
+    korean_search_results_doubt = get_search_results(user_input_doubt, serpapi_api_key, language="ko")
 
     # 영어 검색 결과에서 organic_results 부분 추출
     english_results_text_cause = ' '.join([result['snippet'] for result in english_search_results_cause.get('organic_results', [])])
     english_results_text_disease = ' '.join([result['snippet'] for result in english_search_results_disease.get('organic_results', [])])
+    english_results_text_doubt = ' '.join([result['snippet'] for result in english_search_results_doubt.get('organic_results', [])])
 
     # 한국어 검색 결과에서 'organic_results' 부분 추출
     korean_results_text_cause = ' '.join([result['snippet'] for result in korean_search_results_cause.get('organic_results', [])])
     korean_results_text_disease = ' '.join([result['snippet'] for result in korean_search_results_disease.get('organic_results', [])])
-    # 검색 결과를 통합
-    combined_results = english_results_text_cause+" "+ english_results_text_disease + " " + korean_results_text_cause + " " + korean_results_text_disease
+    korean_results_text_doubt = ' '.join([result['snippet'] for result in korean_search_results_doubt.get('organic_results', [])])
+    
+    # 검색 결과를 통합 후 LLM에게 전달
+    combined_results_disease = korean_results_text_disease + " " + english_results_text_disease 
+    preprocessed_prompt_disease = preprocess_prompt(combined_results_disease)
+    disease_prompt = f"이 증상으로 인한 예상되는 질병은 무엇입니까? 몇가지 알려주세요. 한국어로 답해주세요. \n\n{preprocessed_prompt_disease}"
+    llm_response_disease = agent.run({"input": disease_prompt})
 
-    # 문자열 데이터로부터 전처리된 프롬프트를 생성
-    preprocessed_prompt = preprocess_prompt(combined_results)
-    # 통합된 검색 결과를 LLM에게 전달하여 분석
-    llm_response = agent.run({"input": preprocessed_prompt})
+    combined_results_cause = korean_results_text_cause + " " +  english_results_text_cause
+    preprocessed_prompt_cause = preprocess_prompt(combined_results_cause)
+    cause_prompt = f"이 증상의 예상되는 원인은 무엇입니까? 한국어로 답해주세요. \n\n{preprocessed_prompt_cause}"
+    llm_response_cause = agent.run({"input": cause_prompt})
+
+    combined_results_doubt = korean_results_text_doubt + " " +  english_results_text_doubt
+    preprocessed_prompt_doubt = preprocess_prompt(combined_results_doubt)
+    doubt_prompt = f"어떤 병원에서 어떤 검사를 받아보는 것이 좋을까요? 몇가지 추천해주시고 한국어로 답해주세요. \n\n{preprocessed_prompt_doubt}"
+    llm_response_doubt = agent.run({"input": doubt_prompt})
 
     # 최종적으로 분석된 결과 출력
-    print(llm_response)
+    print("예상되는 질병 : " + llm_response_disease)
+    print("예상되는 원인 : " + llm_response_cause)
+    print("추천 검사 : " + llm_response_doubt)
     keep_going = input("계속하시겠습니까? : ")
     if(keep_going == "아니오"):
         break

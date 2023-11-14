7fc84c2ab9b171acc244d28fa5b9a9a95bb6a0f5
박동휘 <daniel1502@naver.com>
2023-11-14 11:05:52 +0900

    Feat: 병원 검색 결과 및 병원 상세 페이지 기능 추가 및 수정

--- frontend/src/components/Organisms/Pagination.tsx
+++ frontend/src/components/Organisms/Pagination.tsx
@@ -10,14 +10,24 @@ const Pagination: React.FC<PaginationProps> = ({
   totalPages,
   onPageChange,
 }) => {
+  const isPprevButtonDisabled = pageNum <= 3;
   const isPrevButtonDisabled = pageNum <= 1;
   const isNextButtonDisabled = pageNum >= totalPages;
+  const isNnextButtonDisabled = pageNum >= totalPages - 2;
 
   const startPage = Math.max(pageNum - 2, 1);
   const endPage = Math.min(pageNum + 2, totalPages);
 
   return (
     <div className="pagination-container">
+      <span
+        className={`pagination-arrow ${
+          isPprevButtonDisabled ? "disabled" : ""
+        }`}
+        onClick={() => onPageChange(1)}
+      >
+        &lt;&lt;
+      </span>
       <span
         className={`pagination-arrow ${isPrevButtonDisabled ? "disabled" : ""}`}
         onClick={() => onPageChange(pageNum - 1)}
@@ -27,7 +37,7 @@ const Pagination: React.FC<PaginationProps> = ({
       <div>
         {Array.from(
           { length: endPage - startPage + 1 },
-          (_, i) => startPage + i,
+          (_, i) => startPage + i
         ).map((page, index) => (
           <span
             key={index}
@@ -45,6 +55,14 @@ const Pagination: React.FC<PaginationProps> = ({
         onClick={() => onPageChange(pageNum + 1)}
       >
         &gt;
+      </span>
+      <span
+        className={`pagination-arrow ${
+          isNnextButtonDisabled ? "disabled" : ""
+        }`}
+        onClick={() => onPageChange(totalPages)}
+      >
+        &gt;&gt;
       </span>
     </div>
   );

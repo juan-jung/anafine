be4cd9b4c0589e48ea0f614ffa4f29d198176d0e
김민우 <jerry142857@naver.com>
2023-11-14 17:07:29 +0900

    Update infoPage

--- frontend/src/pages/info/index.tsx
+++ frontend/src/pages/info/index.tsx
@@ -1,135 +1,60 @@
-import { motion, useScroll } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import Header from "components/Organisms/Header";
-import ShapeImage from "components/atoms/ShapeImage";
+import { useState } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image1 from "../../../public/infoPic/infoImage1.png";
+import Image2 from "../../../public/infoPic/infoImage2.png";
+import Image3 from "../../../public/infoPic/infoImage3.png";
 
-const InfoPage: NextPage = () => {
-  const { scrollYProgress } = useScroll();
+const images = [Image1, Image2, Image3];
+
+const InfoPage = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleChange = (index: number) => {
+    setCurrentIndex(index);
+  };
 
   return (
-    <div className={styles.container}>
+    <div className="flex justify-center items-center py-5 px-3">
       <Head>
         <title>SSAFY A403 자율프로젝트</title>
         <meta name="description" content="SSAFY A403 자율프로젝트" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Header />
-      <motion.div
-        className={"progress-bar"}
-        style={{ scaleX: scrollYProgress }}
-      />
-      <main className={styles.main}>
-        <section className={"info-section1"}>
-          <div className={"info-content"}>
-            <div className={"bubble right"}>급여와 비급여가 무었인가요?</div>
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/man.png"}
-              alt={"남자"}
-              width={300}
-              height={300}
-            />
-          </div>
-          <div className={"info-content"}>
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/pic2.png"}
-              alt={"설명2"}
-              width={350}
-              height={300}
-            />
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/pic3.png"}
-              alt={"설명3"}
-              width={350}
-              height={300}
-            />
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/pic5.png"}
-              alt={"설명5"}
-              width={350}
-              height={300}
-            />
-          </div>
-        </section>
-
-        <section className={"info-section2"}>
-          <div className={"info-content"}>
-            <div className={"bubble right"}>
-              그럼 제가 내야하는 병원비는 어떻게 되나요?
+      <Carousel
+        showArrows={true} // 화살표 표시 여부
+        autoPlay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        selectedItem={currentIndex}
+        onChange={handleChange}
+        className="w-[400px] lg:hidden"
+        renderArrowPrev={(clickHandler, hasPrev, label) =>
+          hasPrev && (
+            <div className="arrow" onClick={clickHandler} title={label} style={{ position: "absolute", top: "50%", left: 15 }}>
+            &lt;
             </div>
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/man.png"}
-              alt={"남자"}
-              width={300}
-              height={300}
-            />
-          </div>
-          <div className={"info-content"}>
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/pic6.png"}
-              alt={"설명6"}
-              width={350}
-              height={300}
-            />
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/pic7.png"}
-              alt={"설명7"}
-              width={350}
-              height={300}
-            />
-          </div>
-        </section>
-
-        <section className={"info-section1"}>
-          <div className={"info-content"}>
-            <div className={"bubble right"}>
-              비급여 항목은 병원마다 가격 차이가 있는데 쉽게 알 수 있는 방법이
-              없을까요?
+          )
+        }
+        renderArrowNext={(clickHandler, hasNext, label) =>
+          hasNext && (
+            <div className="arrow" onClick={clickHandler} title={label} style={{ position: "absolute", top: "50%", right: 15 }}>
+            &gt;
             </div>
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/man.png"}
-              alt={"남자"}
-              width={300}
-              height={300}
-            />
+          )
+        }
+      >
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image.src} alt={`Image ${index + 1}`} />
           </div>
-          <div className={"info-content"}>
-            <ShapeImage
-              shape={"square"}
-              src={"/infoPic/advocate.png"}
-              alt={"여자"}
-              width={300}
-              height={300}
-            />
-            <div>
-              <div className={"bubble left"}>
-                "아나파잉" 서비스를 통해 쉽게 비급여에 관한 병원 및 기타 정보를
-                얻을 수 있어요!
-              </div>
-              <div className={"bubble left"}>
-                비급여 항목은 병원 자체적으로 진료비를 정하기 때문에 금액 차이가
-                크게 발생하게 됩니다!
-              </div>
-            </div>
-          </div>
-          <ShapeImage
-            shape={"square"}
-            src={"/infoPic/pic8.png"}
-            alt={"설명8"}
-            width={350}
-            height={300}
-          />
-        </section>
-      </main>
+        ))}
+      </Carousel>
     </div>
   );
 };

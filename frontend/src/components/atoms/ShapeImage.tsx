15951e48089d471bbbbbfb331da2edb13696982d
박동휘 <daniel1502@naver.com>
2023-11-17 10:29:35 +0900

    Design : Header 검색창 세부 위치 조정

--- frontend/src/components/atoms/ShapeImage.tsx
+++ frontend/src/components/atoms/ShapeImage.tsx
@@ -2,7 +2,13 @@ import styled from "@emotion/styled";
 import Image, { ImageProps } from "next/image";
 
 type ImageShape = "square" | "circle";
-type ShapeImageProps = ImageProps & { shape?: ImageShape };
+type ShapeImageProps = ImageProps & {
+  shape?: ImageShape;
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+};
 
 const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
   border-radius: ${({ shape }) => (shape === "circle" ? "50%" : "0")};
@@ -10,8 +16,24 @@ const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
 `;
 
 // 이미지 형태
-const ShapeImage: React.FC<ShapeImageProps> = ({ shape, ...ImageProps }) => {
-  return <ImageWithShape shape={shape} {...ImageProps} />;
+const ShapeImage: React.FC<ShapeImageProps> = ({
+  shape,
+  src,
+  alt,
+  width,
+  height,
+  ...ImageProps
+}) => {
+  return (
+    <ImageWithShape
+      shape={shape}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      {...ImageProps}
+    />
+  );
 };
 
 export default ShapeImage;
